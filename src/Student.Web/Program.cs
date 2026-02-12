05e5b51cb496e55394235f9e18fc4e9c2685463d
Kishan Aredla <Kishan.Aredla@outlook.com>
2026-02-12 09:07:40 +0530

    Revert "checking"
    This reverts commit 5d3dab29482da71ba414550f62f4a53cab627787.

--- src/Student.Web/Program.cs
+++ src/Student.Web/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddHttpClient<ApiService>(c =>
 });
 
 
-var app = builder.Build():
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
